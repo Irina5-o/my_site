f0ba9e40873feae7539207184700dfc6a2eefa6c
Name <email@example.ru>
2025-11-13 21:52:37 +0200

    feat: Register Project model in admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'link')
+    list_filter = ('title',)
+    search_fields = ('title', 'description')
+
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('title', 'description', 'link')
+        }),
+    )
